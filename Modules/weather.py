c95abb866ab39cfd02b227994bd281ee9d342176
LXDiaz <64718727+LX-Diaz@users.noreply.github.com>
2024-05-12 09:41:46 -0400

    Upload 5/12/24
    Updated Weather data module to try and connect to the weather service. Should it fail, it will return blank values to the program doesn't crash.

--- Modules/weather.py
+++ Modules/weather.py
@@ -15,24 +15,30 @@ class WeatherData():
         self.desc = ''
 
     def getWeatherData(self):
-        self.response = requests.get(self.url)
+        try:
+            self.response = requests.get(self.url)
 
-        if self.response.status_code == 200:
-            self.data = self.response.json()
-            self.main_temp_kelvin = int(self.data['main']['temp'])
-            self.temperature = ((int(self.data['main']['temp'])-273.15)*9//5+32)
-            self.feels_temp = ((int(self.data['main']['feels_like'])-273.15)*9//5+32)
-            self.min_temp = ((int(self.data['main']['temp_min'])-273.15)*9//5+32)
-            self.max_temp = ((int(self.data['main']['temp_max'])-273.15)*9//5+32)
-            self.humidity = int(self.data['main']['humidity'])
-            self.desc = self.data['weather'][0]['description']
-
-            return self.temperature
-
-        else:
-            print('Error fetching weather data')
+            if self.response.status_code == 200:
+                self.data = self.response.json()
+                self.main_temp_kelvin = int(self.data['main']['temp'])
+                self.temperature = ((int(self.data['main']['temp']) - 273.15) * 9 // 5 + 32)
+                self.feels_temp = ((int(self.data['main']['feels_like']) - 273.15) * 9 // 5 + 32)
+                self.min_temp = ((int(self.data['main']['temp_min']) - 273.15) * 9 // 5 + 32)
+                self.max_temp = ((int(self.data['main']['temp_max']) - 273.15) * 9 // 5 + 32)
+                self.humidity = int(self.data['main']['humidity'])
+                self.desc = self.data['weather'][0]['description']
 
 
 
+            else:
+                print('Error fetching weather data')
 
+        except:
+            self.main_temp_kelvin = '00'
+            self.temperature = '00'
+            self.feels_temp = '00'
+            self.min_temp = '00'
+            self.max_temp = '00'
+            self.humidity = '00'
+            self.desc = 'No Data'
 
